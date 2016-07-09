880873d196d3488972050b69ad0e6e43694b6306
SaSaChu <teresa7397@gmail.com>
2016-07-09 18:13:24 +0800

    剩下內頁

--- js/public.js
+++ js/public.js
@@ -1,9 +1,15 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2016 OA Wu Design
- */
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.6&appId=640377126095413";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 
 $(function () {
 
-  // $('.typbox').css ({'border': '1px solid rgba(255, 0, 0, .3)'})
+ $('.adpic, .p01, .x01').imgLiquid ({verticalAlign: 'center'});
+
 });
