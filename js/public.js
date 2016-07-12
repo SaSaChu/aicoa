5de635be1e989f4a4e69a012866b1c606659d45d
SaSaChu <teresa7397@gmail.com>
2016-07-12 11:47:42 +0800

    畫面修正

--- js/public.js
+++ js/public.js
@@ -1,16 +1,17 @@
 
-(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.6";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+$(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.6";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 
 $(function () {
 
   $('.adpic, .p01, .x01').imgLiquid ({verticalAlign: 'center'});
-  
+
   $('#menu > div').click (function () {
     if (!$('#menu').hasClass ('show')) return $('#menu').addClass ('show');
     if ($(this).hasClass ('sp')) return $('#menu').removeClass ('show');
